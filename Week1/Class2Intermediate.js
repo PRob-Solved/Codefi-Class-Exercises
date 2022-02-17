63d2c1b49ceddfc49f28b3cda6ff0daf8f3f1be3
mycommonplaces <47791675+mycommonplaces@users.noreply.github.com>
2022-02-17 17:55:10 -0600

    Clean up

--- Week1/Class2Intermediate.js
+++ Week1/Class2Intermediate.js
@@ -1,20 +1,47 @@
-function intermediateAssignment() {
-    
-    let foodList = document.querySelector(".food-list");
+(function() {
 
-
+    let foodList = document.querySelector('.food-list')
+      
     let foodObjects = [
         {name: "Chicken Fried Steak", price: 9.95},
         {name: "Bacon Double Cheeseburger", price: 8.95},
         {name: "Taco Salad", price: 7.95}
-    ];
+        ];
+        
+    
+    let foodItems = ''
+      
+    for (food of foodObjects) {
+        foodItems += '<li>' + food.name + ', ' + food.price + '</li>'
+      }
+      
+    foodList.innerHTML = foodItems
+    
+    })();
 
-    for (let food of foodObjects) {
-        const foodData = document.createElement('li');
-        foodData.innerHTML = "<li>" + food.name + "</li>";
-        foodList.appendChild(foodData);
-        // foodItems += "<li>" + food.name + "</li>";
-    }
 
-    foodList.innerHTML = foodItems;
-}
+
+
+
+
+
+// function intermediateAssignment() {
+//     let foodList = document.querySelector(".food-list");
+
+//     const foodItems = [
+//         {name: "Chicken Fried Steak", price: 9.95},
+//         {name: "Bacon Double Cheeseburger", price: 8.95},
+//         {name: "Taco Salad", price: 7.95}
+//     ];
+
+//     for (let food of foodItems) {
+//         const foodData = document.createElement('li');
+//         foodData.innerHTML = "<li>" + food.name + "</li>";
+//         foodList.appendChild(foodData);
+//         // foodItems += "<li>" + food.name + "</li>";
+//     }
+
+//     foodList.innerHTML = food;
+// }
+
+// intermediateAssignment();
--- Week1/class2Basic.js
+++ Week1/class2Basic.js
@@ -3,8 +3,8 @@ const string = "string"; // variable that holds a string value
 const number = 7; // variable that holds a number
 const boolean = 7 > 1;
 const nul = null;
-const notdefined;
-const object {
+let notdefined;
+const something = {
     name: "Paul", eats: "okra"
     };
-const array [7, 34, 35];
+const somethingElse = [7, 34, 35];
