f4e88a8d002dfe25419dcb308b325fb2e94ddbe8
mycommonplaces <47791675+mycommonplaces@users.noreply.github.com>
2022-02-21 15:08:20 -0600

    Class 3: Basic and Intermediate

---
+++ Class 3/Class3Intermediate.js
@@ -0,0 +1,11 @@
+let users = [
+
+    {   name: "Peter",
+        age: 38,
+        occupation: "friendly neighborhood SpiderMan"
+    }
+];
+
+function listUsers(user) {
+    console.log(`User: ${users[0].name}, age: ${users[0].age}, occupation: ${users[0].occupation}`);
+}
