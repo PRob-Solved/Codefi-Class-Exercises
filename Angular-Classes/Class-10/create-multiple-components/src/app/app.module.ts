3cee8cd941ec7b39bcbf0f1006a994f0f79c4adc
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-03-22 09:18:51 -0500

    Create and update Exercises 1-5

--- Angular-Classes/Class-10/create-multiple-components/src/app/app.module.ts
+++ Angular-Classes/Class-10/create-multiple-components/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './home/app-home.component';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
--- Angular-Classes/Class-10/dynamic-content/src/app/app.module.ts
+++ Angular-Classes/Class-10/dynamic-content/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomeComponent } from './home/app-home.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
