707efef1ca39eb19e2a9807f6445cbbccfb61efb
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-03-29 12:15:30 -0500

    Update Class-13 Exercises

---
+++ Angular-Classes/Class-13/dynamic-button-event-binding/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dynamic-button-event-binding';
+
+  isDisabled: boolean = true;
+
+  toggleDisable() {
+    this.isDisabled = !this.isDisabled;
+  };
+
+}
